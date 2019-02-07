935c0308cd91c79e7394baffa7a37380f1f047c6
JolanPhilippe <jp2589@wind.nauhpc>
2019-02-06 19:19:52 -0700

    Modif in scripts to ease tests

--- test_prefix.py
+++ test_prefix.py
@@ -7,6 +7,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", help="name of the files that contain the PTree parts")
+parser.add_argument("-nt", help="number of tests to do", type = int)
 args = parser.parse_args()
 
 filename = args.f
@@ -14,12 +15,25 @@ if filename[-3:] != ".pt":
 	filename = filename + ".pt"
 
 pt = PTree.init_from_file(filename)
+ntest = args.nt
 
-comm.barrier()
-start_time = time.time()
-res = prefix(pt)
-comm.barrier()
-end_time = time.time()
+# for i in range(0,nprocs):
+#     MPI.COMM_WORLD.Barrier()
+#     if pid == i:
+#         print ('Rank %d out of %d' % (pid,nprocs))
 
-at_root(lambda : print(str(nprocs) + "\t" + str(format(end_time-start_time, '.6f')) + "\t" + "OK" ))
+
+for i in range(ntest):
+	try:
+		comm.barrier()
+		start_time = time.time()
+		res = prefix(pt)
+		comm.barrier()
+		end_time = time.time()
+		at_root(lambda : print(str(nprocs) + "\t" + str(format(end_time-start_time, '.6f')) + "\t" + "OK" ))
+	except: 
+		comm.barrier()
+		end_time = time.time()
+		at_root(lambda : print(str(nprocs) + "\t" + str(format(end_time-start_time, '.6f')) + "\t" + "KO" ))
+
 
