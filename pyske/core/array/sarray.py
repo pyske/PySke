13181537081a401938a931bfed7715f270cf340d
Frederic Loulergue <frederic@loulergue.eu>
2019-07-10 18:18:31 -0700

    - New data structure: SArray (inherits from array.array) - Observers for SArray: length, is_empty - Constructor for SArray: init - Skeletons for sarray: map, mapi, map2, map2i, zip (returns a SList), reduce, scan, scanl, scanl_last, filter - Tests for all skeletons and functions

--- pyske/core/array/sarray.py
+++ pyske/core/array/sarray.py
@@ -1,33 +1,32 @@
 import functools
 import array
+from pyske.core.list.slist import SList
 
 def code_of_type(x):
     if type(x) == int: return 'i'
     return 'd'
 
-def default_of_code(code):
-    if code == 'i': return 1
-    return 1.0
+def default(code):
+    if code == 'i':
+        return 1
+    else:
+        return 1.0
 
 class SArray(array.array):
 
-    def empty(self):
+    def __default(self):
+        return default(self.typecode)
+
+    @staticmethod
+    def init(f, size):
+        assert 0 <= size
+        return SArray(code_of_type(f(0)), [f(i) for i in range(0, size)])
+
+    def is_empty(self):
         """Indicates if a array is empty
         """
         return self.length() == 0
 
-    def head(self):
-        """Gives the first element of the current instance
-        """
-        if self.empty():
-            return None
-        else:
-            return self[0]
-
-    def tail(self):
-        """Gives the the current instance without its first element
-        """
-        return SArray(self.typecode, self[1:])
 
     def length(self):
         """Gives the number of element in the current instance
@@ -45,7 +44,8 @@ class SArray(array.array):
         f : callable
             The function to apply to every values of the current instance
         """
-        return SArray(code_of_type(type(f(default_of_code(self.typecode)))), [ f(x) for x in self] )
+        return SArray(code_of_type(f(self.__default())), [f(x) for x in self])
+
 
     def mapi(self, f):
         """Applies f to every index and element of the current instance
@@ -58,5 +58,58 @@ class SArray(array.array):
         f: callable
         The function to apply to every index and element of the current instance
         """
-        return SArray(code_of_type(type(f(default_of_code(self.typecode), 0))),
-                                   [f(i, self[i]) for i in range(0, len(self))])
+        return SArray(code_of_type(f(0, self.__default())),
+                      [f(i, self[i]) for i in range(0, len(self))])
+
+
+    def map2(self, f, a):
+        assert len(a) == len(self)
+        return SArray(code_of_type(f(self.__default(), a.__default())),
+                      [f(self[i], a[i]) for i in range(0, len(self))])
+
+
+    def map2i(self, f, a):
+        assert len(a) == len(self)
+        return SArray(code_of_type(f(0, self.__default(), a.__default())),
+                      [f(i, self[i], a[i]) for i in range(0, len(self))])
+
+    def zip(self, a):
+        return SList([(self[i], a(i)) for i in range(0, len(self))])
+
+
+    def reduce(self, op, e=None):
+        assert(not(e is None) or 0 < len(self))
+        if e is None:
+            return functools.reduce(op, self)
+        else:
+            return functools.reduce(op, self, e)
+
+    def scan(self, f, c):
+        tc = code_of_type(c)
+        res = array.array(tc)
+        res.append(c)
+        for i in range(1, len(self)+1):
+            c = f(c, self[i - 1])
+            res.append(c)
+        return SArray(tc, res)
+
+    def scanl(self, f, c):
+        tc = code_of_type(c)
+        res = array.array(tc)
+        for i in range(0, len(self)):
+            res.append(c)
+            c = f(c, self[i])
+        return SArray(tc, res)
+
+    def scanl_last(self, f, c):
+        tc = code_of_type(c)
+        res = array.array(tc)
+        for i in range(0, len(self)):
+            res.append(c)
+            c = f(c, self[i])
+        return (SArray(tc, res), c)
+
+    def filter(self, p):
+        tc = self.typecode
+        return SArray(tc, filter(p, self))
+
--- pyske/test/array/test_sarray.py
+++ pyske/test/array/test_sarray.py
@@ -1,96 +1,76 @@
-from pyske.core.support.errors import NotEqualSizeError, EmptyError
 from pyske.core.array.sarray import SArray
+from operator import add
 import pytest
+pytestmark = pytest.mark.sarray
 
+def id(x):
+    return x
 
-# -------------------------- #
-
-def test_head_empty():
-    sl = SArray('i')
-    exp = None
-    res = sl.head()
-    assert res == exp
-
-
-def test_head_one():
-    sl = SArray('d')
-    exp = 1
-    sl.append(exp)
-    res = sl.head()
-    assert res == exp
-
-
-def test_head_several():
-    sl = SArray('i')
-    exp = 1
-    sl.append(exp)
-    sl.append(exp + 1)
-    res = sl.head()
-    assert res == exp
-
+def incr(x):
+    return x + 1
 
 # -------------------------- #
 
-def test_tail_empty():
-    sl = SArray('i')
-    exp = SArray('i')
-    res = sl.tail()
-    assert res == exp
-
-
-def test_tail_one():
-    sl = SArray('d', [1])
-    exp = SArray('d')
-    res = sl.tail()
-    assert res == exp
-
-
-def test_tail_several():
-    sl = SArray('d', [1, 2, 3])
-    exp = SArray('d', [2, 3])
-    res = sl.tail()
-    assert res == exp
-
-
-# -------------------------- #
-
-def test_length_nil():
-    sl = SArray('d')
+def test_length_empty():
+    sl = SArray('f')
     exp = 0
     res = sl.length()
     assert res == exp
 
 
-def test_length_cons():
-    sl = SArray('i', [1, 2, 3])
-    exp = 3
+def test_length_non_empty():
+    size = 11
+    sl = SArray('i', range(0,size))
+    exp = size
     res = sl.length()
     assert res == exp
 
+# -------------------------- #
+
+def test_init_empty():
+    res = SArray.init(id, 0)
+    exp = SArray('i')
+    assert res == exp
+
+
+def test_init_non_empty():
+    size = 11
+    res = SArray.init(incr, size)
+    exp = SArray('i', range(1, size+1))
+    assert res == exp
+
+# -------------------------- #
+
+def test_is_empty_empty():
+    res = SArray.init(id, 0).is_empty()
+    assert res
+
+
+def test_is_empty_non_empty():
+    size = 11
+    res = SArray.init(incr, size).is_empty()
+    assert not res
 
 # -------------------------- #
 
 def test_map_empty():
     sl = SArray('d')
     exp = SArray('d')
-    f = lambda x: x
-    res = sl.map(f)
+    res = sl.map(id)
     assert res == exp
 
 
-def test_map_inc():
+def test_map_incr():
     sl = SArray('i', [1, 2, 3])
     exp = SArray('i', [2, 3, 4])
-    f = lambda x: x + 1
-    res = sl.map(f)
+    res = sl.map(incr)
     assert res == exp
 
 
 def test_map_id():
     sl = SArray('f', [1, 2, 3])
     exp = SArray('f', [1, 2, 3])
-    f = lambda x: x
-    res = sl.map(f)
+    res = sl.map(id)
     assert res == exp
 
 # -------------------------- #
@@ -116,4 +96,98 @@ def test_mapi_id():
     exp = SArray('f', [1, 2, 3])
     f = lambda i, x: x
     res = sl.mapi(f)
+    assert res == exp
+
+# -------------------------- #
+
+def test_map2_empty():
+    input = SArray('i')
+    exp = SArray('i')
+    res = input.map2(add, input)
+    assert res == exp
+
+
+def test_map2_non_empty():
+    input = SArray('i', [1, 2, 3])
+    exp = SArray('i', [2, 4, 6])
+    res = input.map2(add, input)
+    assert res == exp
+
+# -------------------------- #
+
+def test_reduce_empty():
+    input = SArray('f')
+    c = 42.0
+    res = input.reduce(add, c)
+    exp = c
+    assert res == exp
+
+def test_reduce_non_empty():
+    size = 111
+    input = SArray.init(id, size)
+    res = input.reduce(add)
+    exp = int(((size-1)*size ) / 2)
+    assert res == exp
+
+# -------------------------- #
+
+def test_scan_empty():
+    input = SArray('f')
+    c = 42.0
+    res = input.scan(add, c)
+    exp = SArray('f', [c])
+    assert res == exp
+
+def test_scan_non_empty():
+    size = 111
+    input = SArray.init(lambda x: 1, size)
+    res = input.scan(add, 0)
+    exp = SArray.init(id, size+1)
+    assert res == exp
+
+# -------------------------- #
+
+def test_scanl_empty():
+    input = SArray('f')
+    c = 42.0
+    res = input.scanl(add, c)
+    exp = input
+    assert res == exp
+
+def test_scanl_non_empty():
+    size = 111
+    input = SArray.init(lambda x: 1, size)
+    res = input.scanl(add, 0)
+    exp = SArray.init(id, size)
+    assert res == exp
+
+# -------------------------- #
+
+def test_scanl_last_empty():
+    input = SArray('f')
+    c = 42.0
+    res = input.scanl_last(add, c)
+    exp = (input, c)
+    assert res == exp
+
+def test_scanl_last_non_empty():
+    size = 111
+    input = SArray.init(lambda x: 1, size)
+    res = input.scanl_last(add, 0)
+    exp = (SArray.init(id, size), 111)
+    assert res == exp
+
+# -------------------------- #
+
+def test_filter_empty():
+    input = SArray('f')
+    res = input.filter(lambda x: x == 0.0)
+    exp = input
+    assert res == exp
+
+def test_filter_non_empty():
+    size = 111
+    input = SArray.init(lambda x: float(x), size)
+    res = input.filter(lambda x: x != 0.0)
+    exp = SArray.init(lambda x: float(x+1), size-1)
     assert res == exp
