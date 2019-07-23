56d0d9df75a613a16215bc180b23a51c649679f0
Frederic Loulergue <frederic@loulergue.eu>
2019-07-23 09:02:59 -0700

    Remove runnable from master

--- pyske/core/runnable/run.py
+++
@@ -1,9 +0,0 @@
-from pyske.core.runnable.rule.rule_slist import *
-from pyske.core.runnable.rule.rule_plist import *
-from pyske.core.runnable.transformation import transformation
-
-rules = rules_slist + rules_plist
-
-def run(t, rules = rules):
-    t = transformation(t, rules)
-    return t.run()
