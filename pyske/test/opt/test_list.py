ac19c0127c07937f3004606a4b88046d2b055a10
Frederic Loulergue <frederic@loulergue.eu>
2019-07-28 02:57:41 -0700

    more refactoring after pylinting

--- pyske/test/opt/test_list.py
+++ pyske/test/opt/test_list.py
@@ -1,20 +1,26 @@
+"""
+Tests for the optimization mechanism on lists
+"""
+from operator import add
 import pytest
 from pyske.core.opt.list import PList, SList
 from pyske.core.list.plist import PList as DPList
 from pyske.core.list.slist import SList as DSList
-from operator import add
 from pyske.core.util.fun import incr
 
+# pylint: disable=invalid-name
 pytestmark = pytest.mark.opt_list
 
 
 def test_plist():
+    # pylint: disable=missing-docstring
     exp = DPList.init(id, 10).map(incr).map(incr).reduce(add)
     res = PList.init(id, 10).map(incr).map(incr).reduce(add).run()
     assert res == exp
 
 
 def test_slist():
+    # pylint: disable=missing-docstring
     exp = DSList.init(id, 10).map(incr).map(incr).reduce(add)
     res = SList.init(id, 10).map(incr).map(incr).reduce(add).run()
     assert res == exp
