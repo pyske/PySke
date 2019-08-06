3309c4c3a4d67dfa6f365a79a9ba30d5d50701d2
Frederic Loulergue <frederic@loulergue.eu>
2019-08-06 09:37:39 -0700

    - refactoring of the dot product example

--- pyske/examples/list/dot_product.py
+++ pyske/examples/list/dot_product.py
@@ -2,82 +2,78 @@
 Dot product of two vectors implemented as parallel lists
 """
 
-import random
-import argparse
-import gc
 from operator import add, mul
 from pyske.core import fun, par, Timing, PList as DPList
 from pyske.core.opt import fun as opt
 from pyske.core.opt.list import PList
 
-__all__ = []
+__all__ = ['dot_product']
 
 
-def __rand(_):
-    return random.randint(0, 100)
+# ------------------- Dot Product Example -------------------
 
 
-def __wrap(test):
-    if test in ['direct', 'hand_optimized']:
-        return fun.idt
-    return PList.raw
+def dot_product(vector1, vector2, uncurry=None):
+    """
+    Compute the dot product of two vectors.
+
+    :param vector1: list of numbers representing a vector
+    :param vector2: list of numbers representing a vector
+    :param uncurry: (optional)
+    :return: the dot product of the two vectors
+    """
+    if uncurry is None:
+        uncurry = fun.uncurry
+    return vector1.zip(vector2).map(uncurry(mul)).reduce(add, 0)
 
 
-def __dot_product(wrapper, uncurry, pl1: DPList, pl2: DPList):
-    new_pl1 = wrapper(pl1)
-    new_pl2 = wrapper(pl2)
-    return new_pl2.zip(new_pl1).map(uncurry(mul)).reduce(add, 0)
-
-
-def __print_info(iteration, result, max_value, avg_value, all_values):
-    return lambda: \
-        print(f'Iteration:\t{iteration}\n'
-              f'Result:\t{result}\n'
-              f'Time (max):\t{max_value}\n'
-              f'Time (avg):\t{avg_value}\n'
-              f'Time (all):\t{all_values}')
-
-
-def __compute(test, pl1, pl2):
-    uncurry = fun.uncurry if test in ['direct', 'hand_optimized'] else opt.uncurry
-    wrapper = __wrap(test)
-    if test == 'direct':
-        return __dot_product(wrapper, uncurry, pl1, pl2)
-    if test == 'wrapper':
-        return __dot_product(wrapper, uncurry, pl1, pl2).eval()
-    if test == 'optimized':
-        return __dot_product(wrapper, uncurry, pl1, pl2).run()
-    if test == 'hand_optimized':
-        return pl2.map2(mul, pl1).reduce(add, 0)
-    return DPList()
-
+# -------------- Execution -----------------
 
 def __main():
-    time = Timing()
-    random.seed(42)
+    import argparse
+    import gc
+    from pyske.examples.list.util import rand_list, print_experiment
+
+    def __compute():
+        if test == _DIRECT:
+            return dot_product(pl1, pl2)
+        if test == _HAND:
+            return pl2.map2(mul, pl1).reduce(add, 0)
+        if test == _EVAL:
+            return dot_product(PList.raw(pl1), PList.raw(pl2), uncurry=opt.uncurry).eval()
+        return dot_product(PList.raw(pl1), PList.raw(pl2), uncurry=opt.uncurry).run()
+
     # Command-line arguments parsing
     parser = argparse.ArgumentParser()
     parser.add_argument("--size", help="size of the list to generate", type=int, default=1_000_000)
     parser.add_argument("--iter", help="number of iterations", type=int, default=30)
     parser.add_argument("--test", help="choice of the test",
-                        choices=['direct', 'wrapper', 'optimized', 'hand_optimized'],
-                        default='direct')
+                        choices=[_DIRECT, _HAND, _EVAL, _OPT],
+                        default=_DIRECT)
     args = parser.parse_args()
     size = args.size
     test = args.test
+
     # Creation of input lists
-    pl1 = DPList.init(__rand, size)
-    pl2 = DPList.init(__rand, size)
+    pl1 = rand_list(DPList, size)
+    pl2 = rand_list(DPList, size)
+
+    # Execution and timing
+    time = Timing()
     par.at_root(lambda: print("Test:\t", test))
     for iteration in range(0, args.iter):
         gc.collect()
+        gc.disable()
         par.barrier()
         time.start()
-        result = __compute(test, pl1, pl2)
+        result = __compute()
         time.stop()
-        max_elapsed, avg_elapsed, all_elapsed = time.get()
-        par.at_root(__print_info(iteration, result, max_elapsed, avg_elapsed, all_elapsed))
+        print_experiment(result, time.get(), par.at_root, iteration)
 
 
 if __name__ == '__main__':
+    _DIRECT = '_DIRECT'
+    _HAND = 'hand_optimized'
+    _OPT = 'optimized'
+    _EVAL = 'evaluated'
     __main()
