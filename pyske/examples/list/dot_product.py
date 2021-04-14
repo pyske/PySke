15c7c2f3474056eb7917820158327efdc3a6b4a3
Frederic Loulergue <frederic@loulergue.eu>
2021-04-14 15:55:21 +0200

    [DOC] Documentation example in dot_product.py

--- pyske/examples/list/dot_product.py
+++ pyske/examples/list/dot_product.py
@@ -9,14 +9,30 @@ from operator import add, mul
 from pyske.core import fun, par, Timing, PList as DPList
 from pyske.core.opt import fun as opt
 from pyske.core.opt.list import PList
+from pyske.examples.list.util import rand_list, print_experiment
 
-__all__ = ['dot_product']
+
+__all__ = ['opt_dot_product', 'dot_product']
+
+
+# ------------------- Dot Product Variant Example -------------
+
+
+def dot_product(vector1, vector2):
+    """
+    Compute the dot product of two vectors.
+
+    :param vector1: list of numbers representing a vector
+    :param vector2: list of numbers representing a vector
+    :return: the dot product of the two vectors
+    """
+    return vector1.map2(mul, vector2).reduce(add, 0)
 
 
 # ------------------- Dot Product Example -------------------
 
 
-def dot_product(vector1, vector2, uncurry=None):
+def opt_dot_product(vector1, vector2, uncurry=fun.uncurry):
     """
     Compute the dot product of two vectors.
 
@@ -24,25 +40,37 @@ def dot_product(vector1, vector2, uncurry=None):
     :param vector2: list of numbers representing a vector
     :param uncurry: (optional)
     :return: the dot product of the two vectors
+
+    Examples::
+
+        >>> from pyske.core import PList
+        >>> vector1 = PList.init(lambda x: x, 10)
+        >>> vector2 = PList.init(lambda x: 1, 10)
+        >>> dot_product(vector1, vector2)
+        45
+
+        >>> from pyske.core import PList
+        >>> vector1 = PList.init(lambda x: x, 10)
+        >>> vector2 = PList.init(lambda x: 9 - x, 10)
+        >>> dot_product(vector1, vector2)
+        120
+
     """
-    if uncurry is None:
-        uncurry = fun.uncurry
     return vector1.zip(vector2).map(uncurry(mul)).reduce(add, 0)
 
 
 # -------------- Execution -----------------
-from pyske.examples.list.util import rand_list, print_experiment
+
 
 def __main():
-
     def __compute():
         if test == _DIRECT:
-            return dot_product(pl1, pl2)
+            return opt_dot_product(pl1, pl2)
         if test == _HAND:
-            return pl2.map2(mul, pl1).reduce(add, 0)
+            return dot_product(pl1, pl2)
         if test == _EVAL:
-            return dot_product(PList.raw(pl1), PList.raw(pl2), uncurry=opt.uncurry).eval()
-        return dot_product(PList.raw(pl1), PList.raw(pl2), uncurry=opt.uncurry).run()
+            return opt_dot_product(PList.raw(pl1), PList.raw(pl2), uncurry=opt.uncurry).eval()
+        return opt_dot_product(PList.raw(pl1), PList.raw(pl2), uncurry=opt.uncurry).run()
 
     # Command-line arguments parsing
     parser = argparse.ArgumentParser()
