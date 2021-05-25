2e23ca359fa565dbe776f3ea74c1ad5fb64def57
Frederic Loulergue <frederic@loulergue.eu>
2021-05-25 13:14:18 +0200

    [Renaming] to maximum_segment_sum, and various renaming and comments for pylinting.

---
+++ pyske/examples/list/maximum_segment_sum_main.py
@@ -0,0 +1,8 @@
+"""
+Execution of maximum_segment_sum.py
+"""
+from pyske.examples.list.maximum_segment_sum import maximum_segment_sum
+from pyske.examples.list.util import standard_main
+
+if __name__ == '__main__':
+    standard_main(maximum_segment_sum)
--- pyske/examples/list/maximum_subarray_problem_main.py
+++
@@ -1,8 +0,0 @@
-"""
-Execution of maximum_subarray_problem.py
-"""
-from pyske.examples.list.maximum_subarray_problem import max_subarray_problem_
-from pyske.examples.list.util import standard_main
-
-if __name__ == '__main__':
-    standard_main(max_subarray_problem_)
