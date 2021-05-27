9f1e0fcead895f6aa292a0e497f56a99506e2306
Besnard Clement <clement.besnard2@etu.univ-orleans.fr>
2021-05-27 14:39:37 +0200

    FIX: bad list initialization parallel list

--- pyske/examples/list/util.py
+++ pyske/examples/list/util.py
@@ -102,11 +102,13 @@ def rand_point_list(cls, size, clusters):
     :return: a list of the given class
     """
     from pyske.core.util.point import Point
-    print(clusters)
-    x, y_true = make_blobs(n_samples=size, centers=clusters)
+    from pyske.core import Distribution
+
+    x, _ = make_blobs(n_samples=size, centers=clusters)
     x = x.tolist()
     x = list(map(lambda y: Point(y[0], y[1]), x))
-    return cls.init(lambda i: x[i], size)
+    distr = Distribution().balanced(size)
+    return cls.from_seq(x).distribute(distr)
 
 
 def print_experiment(result, timing, execute, iteration=None):
