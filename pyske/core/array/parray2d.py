2acc26a07d4250d65023d911866ab13776b98197
Besnard Clement <clement.besnard2@etu.univ-orleans.fr>
2021-06-21 11:27:13 +0200

    use of enum for distribution choice

--- pyske/core/array/parray2d.py
+++ pyske/core/array/parray2d.py
@@ -4,12 +4,41 @@ A module of parallel arrays and associated skeletons
 class PArray2D: parallel arrays.
 """
 from typing import Callable
+from enum import Enum
 
 from pyske.core.support import parallel as parimpl
 
 _PID: int = parimpl.PID
 _NPROCS: int = parimpl.NPROCS
 _COMM = parimpl.COMM
+
+class Distribution(Enum):
+    LINE = 'LINE'
+    COLUMN = 'COLUMN'
+
+
+def _local_index(distribution: str, col_size: int, line_size: int):
+    if distribution == Distribution.LINE:
+        local_size = parimpl.local_size(_PID, line_size)
+        b_start_index = 0
+        b_stop_index = col_size - 1
+    else:
+        local_size = parimpl.local_size(_PID, col_size)
+        b_start_index = 0
+        b_stop_index = line_size - 1
+
+    if _PID == 0:
+        a_start_index = local_size * _PID
+    else:
+        a_start_index = _COMM.recv(source=_PID - 1) + 1
+    a_stop_index = a_start_index + local_size - 1
+    if _PID != _NPROCS - 1:
+        _COMM.send(a_stop_index, _PID + 1)
+
+    if distribution == Distribution.LINE:
+        return (a_start_index, a_stop_index), (b_start_index, b_stop_index)
+    else:
+        return (b_start_index, b_stop_index), (a_start_index, a_stop_index)
 
 
 class PArray2D:
@@ -19,10 +48,11 @@ class PArray2D:
     """
 
     def __init__(self):
-        self.__global_index = 0
-        self.__local_index = 0
+        self.__global_index = ((-1, -1), (-1, -1))
+        self.__local_index = ((-1, -1), (-1, -1))
         self.__content = []
         self.__distribution = [0 for _ in range(0, _NPROCS)]
+        self.__distribution_direction = Distribution.LINE
 
     def __str__(self) -> str:
         return "PID[" + str(_PID) + "]:\n" + \
@@ -39,50 +69,28 @@ class PArray2D:
         parray2d = PArray2D()
         parray2d.__global_index = ((0, line_size - 1), (0, col_size - 1))
 
-        local_line_size = parimpl.local_size(_PID, line_size)
-        if _PID == 0:
-            lines_start_index = local_line_size * _PID
-        else:
-            lines_start_index = _COMM.recv(source=_PID - 1) + 1
-        lines_stop_index = lines_start_index + local_line_size - 1
-        if _PID != _NPROCS - 1:
-            _COMM.send(lines_stop_index, _PID + 1)
+        parray2d.__local_index = _local_index(Distribution.LINE, col_size, line_size)
 
-        colums_start_index = 0
-        colums_stop_index = col_size - 1
-
-        parray2d.__local_index = ((lines_start_index, lines_stop_index),
-                                  (colums_start_index, colums_stop_index))
-
-        parray2d.__content = [value_at(i) for i in range(lines_start_index * col_size,
-                                                         (lines_stop_index + 1) * col_size)]
+        parray2d.__content = [value_at(i) for i in range(parray2d.__local_index[0][0] * col_size,
+                                                         (parray2d.__local_index[0][1] + 1) * col_size)]
         parray2d.__distribution = _COMM.allgather(parray2d.__local_index)
 
         return parray2d
 
     def distribute(self: 'PArray2D') -> 'PArray2D':
+        """
+        Distribute line to column
+        """
         parray2d = PArray2D()
         parray2d.__global_index = self.__global_index
 
         col_size = self.__global_index[1][1] - self.__global_index[1][0] + 1
         line_size = self.__global_index[0][1] - self.__global_index[0][0] + 1
 
-        local_col_size = parimpl.local_size(_PID, col_size)
-        if _PID == 0:
-            colums_start_index = local_col_size * _PID
-        else:
-            colums_start_index = _COMM.recv(source=_PID - 1) + 1
-        colums_stop_index = colums_start_index + local_col_size - 1
-        if _PID != _NPROCS - 1:
-            _COMM.send(colums_stop_index, _PID + 1)
-
-        lines_start_index = 0
-        lines_stop_index = line_size - 1
-
-        parray2d.__local_index = ((lines_start_index, lines_stop_index),
-                                  (colums_start_index, colums_stop_index))
+        parray2d.__local_index = _local_index(Distribution.COLUMN, col_size, line_size)
 
         parray2d.__distribution = _COMM.allgather(parray2d.__local_index)
+        parray2d.__distribution_direction = Distribution.COLUMN
 
         # update content for each process
         for i in range(0, _NPROCS):
