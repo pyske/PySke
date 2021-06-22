efcd451188a9fc2cffa4ed6c310c995cdb4a3797
Besnard Clement <clement.besnard2@etu.univ-orleans.fr>
2021-06-22 10:34:56 +0200

    map function, merge of init function, generic type

--- pyske/core/array/parray2d.py
+++ pyske/core/array/parray2d.py
@@ -3,7 +3,7 @@ A module of parallel arrays and associated skeletons
 
 class PArray2D: parallel arrays.
 """
-from typing import Callable
+from typing import Callable, TypeVar, Generic
 from enum import Enum
 
 from pyske.core import SList
@@ -12,6 +12,9 @@ from pyske.core.support import parallel as parimpl
 _PID: int = parimpl.PID
 _NPROCS: int = parimpl.NPROCS
 _COMM = parimpl.COMM
+
+T = TypeVar('T')  # pylint: disable=invalid-name
+V = TypeVar('V')  # pylint: disable=invalid-name
 
 class Distribution(Enum):
     LINE = 'LINE'
@@ -30,20 +33,20 @@ def _local_index(distribution: Enum, col_size: int, line_size: int, pid: int):
     return (0, line_size - 1), (start_indexes[pid], start_indexes[pid] + local_sizes[pid] - 1)
 
 
-class PArray2D:
+class PArray2D(Generic[T]):
     # pylint: disable=protected-access
     """
     Distributed arrays
     """
 
-    def __init__(self):
+    def __init__(self: 'PArray2D[T]'):
         self.__global_index = ((-1, -1), (-1, -1))
         self.__local_index = ((-1, -1), (-1, -1))
         self.__content = []
-        self.__distribution = [0 for _ in range(0, _NPROCS)]
+        self.__distribution = [((-1, -1), (-1, -1)) for _ in range(0, _NPROCS)]
         self.__distribution_direction = Distribution.LINE
 
-    def __str__(self) -> str:
+    def __str__(self: 'PArray2D[T]') -> str:
         return "PID[" + str(_PID) + "]:\n" + \
                "  global_index: " + str(self.__global_index) + "\n" + \
                "  local_index: " + str(self.__local_index) + "\n" + \
@@ -51,12 +54,16 @@ class PArray2D:
                "  content: " + str(self.__content) + "\n"
 
     @staticmethod
-    def init_line(value_at: Callable[[int, int], int], col_size: int = _NPROCS,
-                  line_size: int = _NPROCS):
+    def init(value_at: Callable[[int, int], V], distribution: Distribution,
+             col_size: int = _NPROCS,
+             line_size: int = _NPROCS) -> 'PArray2D[V]':
         """
         Return an array built using a function per line on each processor
 
         :param value_at: binary function
+        :param distribution: the distribution direction (LINE, COLUMN)
+        :param col_size: number of columns
+        :param line_size: number of lines
         :return: an 2d array of the given line and column size, where for all valid line column
             i, j, the value at this index is value_at(i, j)
         """
@@ -66,48 +73,19 @@ class PArray2D:
         parray2d = PArray2D()
         parray2d.__global_index = ((0, line_size - 1), (0, col_size - 1))
 
-        parray2d.__local_index = _local_index(Distribution.LINE, col_size, line_size, _PID)
+        parray2d.__local_index = _local_index(distribution, col_size, line_size, _PID)
 
         for line in range(parray2d.__local_index[0][0], parray2d.__local_index[0][1] + 1):
             for column in range(parray2d.__local_index[1][0], parray2d.__local_index[1][1] + 1):
                 parray2d.__content.append(value_at(line, column))
-
+        parray2d.__distribution_direction = Distribution.LINE
         parray2d.__distribution = [
             _local_index(parray2d.__distribution_direction, col_size, line_size, i) for i in
             range(_NPROCS)]
 
         return parray2d
 
-    @staticmethod
-    def init_column(value_at: Callable[[int, int], int], col_size: int = _NPROCS,
-                    line_size: int = _NPROCS):
-        """
-        Return an array built using a function per column on each processor
-
-        :param value_at: binary function
-        :return: an 2d array of the given line and column size, where for all valid line column
-            i, j, the value at this index is value_at(i, j)
-        """
-        assert _NPROCS <= col_size
-        assert _NPROCS <= line_size
-
-        parray2d = PArray2D()
-        parray2d.__global_index = ((0, line_size - 1), (0, col_size - 1))
-
-        parray2d.__local_index = _local_index(Distribution.COLUMN, col_size, line_size, _PID)
-
-        for line in range(parray2d.__local_index[0][0], parray2d.__local_index[0][1] + 1):
-            for column in range(parray2d.__local_index[1][0], parray2d.__local_index[1][1] + 1):
-                parray2d.__content.append(value_at(line, column))
-
-        parray2d.__distribution_direction = Distribution.COLUMN
-        parray2d.__distribution = [
-            _local_index(parray2d.__distribution_direction, col_size, line_size, i) for i in
-            range(_NPROCS)]
-
-        return parray2d
-
-    def distribute(self: 'PArray2D') -> 'PArray2D':
+    def distribute(self: 'PArray2D[T]') -> 'PArray2D[T]':
         """
         Distribute line to column
         """
@@ -138,3 +116,13 @@ class PArray2D:
                 _COMM.gather(content_to_send, i)
 
         return parray2d
+
+    def map(self: 'PArray2D[T]', unary_op: Callable[[T], V]) -> 'PArray2D[V]':
+        """
+        Apply a function to all the elements.
+
+        The returned array has the same shape (same size, same distribution)
+        than the initial array.
+        """
+        self.__content = [unary_op(elem) for elem in self.__content]
+        return self
--- pyske/examples/array/hello_world.py
+++ pyske/examples/array/hello_world.py
@@ -2,7 +2,7 @@
 Example: various manipulations on a parallel array
 """
 
-from pyske.core.array.parray2d import PArray2D
+from pyske.core.array.parray2d import PArray2D, Distribution
 
 
 def __main():
@@ -10,7 +10,7 @@ def __main():
     line_size = 12
 
     print("Line initialization")
-    parray2d_line_init = PArray2D.init_line(lambda line, column: line * col_size + column, col_size, line_size)
+    parray2d_line_init = PArray2D.init(lambda line, column: line * col_size + column, Distribution.LINE, col_size, line_size)
     print(parray2d_line_init)
 
     print("Line to column distribution")
@@ -18,7 +18,7 @@ def __main():
     print(parray2d_column)
 
     print("Column initialization")
-    parray2d_column_init = PArray2D.init_column(lambda line, column: line * col_size + column, col_size, line_size)
+    parray2d_column_init = PArray2D.init(lambda line, column: line * col_size + column, Distribution.COLUMN, col_size, line_size)
     print(parray2d_column_init)
 
 
